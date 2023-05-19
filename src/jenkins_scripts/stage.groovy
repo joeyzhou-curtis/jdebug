cf5f3d1adab5b7ceb40ef8d5cd6893885db71db8
joeyzhou-curtis <jieming.zhou@tigergraph.com>
2023-05-19 17:27:52 +0800

    fix path

--- src/jenkins_scripts/stage.groovy
+++ src/jenkins_scripts/stage.groovy
@@ -1,5 +1,5 @@
 
-def update_user_name(os, unit_tests, integration_tests, test_id) {
+def update_user_name() {
     if (JOB_ID =~ "HOURLY" && integration_tests =~ "operator") {
         def operator_owner = UTIL.run_cmd_get_stdout("jq -r '.special_test_check.owner | @tsv' ${test_config_file} | tr '\t' ',' | tr -d '\n'")
         println "operator_owner: $operator_owner"
